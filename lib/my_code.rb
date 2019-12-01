50d7665644c2947d09d275df9ae5b02f65c4986f
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 23:35:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,7 +29,7 @@ def reduce_to_total(source_array,starting_point=0)
   (starting_point...source_array.length).each do |idx|
     total+=source_array[idx]
   end
-  puts "Array = " + source_array
+  puts "Array = " + source_array.to_s
   puts "starting point = " + starting_point.to_s
   puts "total = " + total.to_s
   total
