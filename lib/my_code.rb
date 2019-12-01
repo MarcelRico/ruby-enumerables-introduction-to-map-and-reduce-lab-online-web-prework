572a311ba7b97692bea3fe117c883cdeb20a0dff
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 23:35:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,6 +25,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array,starting_point=0)
+  if(starting_point)
   total = 0
   (starting_point...source_array.length).each do |idx|
     total+=source_array[idx]
