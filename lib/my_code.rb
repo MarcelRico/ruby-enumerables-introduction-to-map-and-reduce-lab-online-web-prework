39005c04b74cde775fbe89a114b692a0abc868a8
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 23:48:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,7 +25,8 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array,starting_point=0)
-  puts "\n" + source_array.to_s
+  puts "\nSource Array = " + source_array.to_s
+  puts "Starting Point = " + starting_point.to_s + "\n\n"
 end
 
 def reduce_to_all_true(source_array)
