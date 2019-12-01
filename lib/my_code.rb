5e672ef22378cf259c9749d47a2b36b4285925cd
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 23:42:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,3 +31,10 @@ def reduce_to_total(source_array,starting_point=0)
   end
   grand_total
 end
+
+def reduce_to_all_true(source_array)
+  source_array.length.times do |idx|
+    return false if source_array[idx]==false
+  end
+  return true
+end
