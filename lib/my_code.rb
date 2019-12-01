c131710cf6eb87ddd37f4972fc3c7da9d603f5bf
Marcel Rico <personal.mr4@gmail.com>
2019-12-01 23:23:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,10 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  source_array.length.times do |idx|
+    source_array[idx]*=2
+  end
+  source_array
+end
